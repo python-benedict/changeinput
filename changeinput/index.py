6ad6ad5cd78f9d32b8a40ca8c17411c99ac0958a
Benedict Osei Sefa <newlyf38@gmail.com>
2022-05-02 11:08:43 +0800

    first commit

---
+++ changeinput/index.py
@@ -0,0 +1,19 @@
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+c = [a,b]
+c.reverse()
+
+a = c[0]
+b = c[1]
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
